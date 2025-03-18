9717bdff7a43f4125b40da0e59c7446a43eda866
Eric <rozanov7475@yandex.by>
2025-03-19 00:09:24 +0300

    load tests added

--- tests/tasks/load/test_get_tasks.py
+++ tests/tasks/load/test_get_tasks.py
@@ -1,9 +1,27 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 
-class HelloWorldUser(HttpUser):
-    host = "http://0.0.0.0:80"
+class BaseTaskUser(HttpUser):
+    wait_time = between(1, 3)
+    task_id = None
+    abstract = True
 
-    @task
-    def hello_world(self):
+    def on_start(self):
+        """Создаём тестовую задачу перед началом тестов."""
+        response = self.client.post("/tasks", json={"name": "Test Task"})
+        if response.status_code == 201:
+            self.task_id = response.json()
+        else:
+            self.task_id = None
+
+    def on_stop(self):
+        """Удаляем задачу по завершении теста (если она осталась)."""
+        if self.task_id:
+            self.client.delete(f"/tasks/{self.task_id}")
+
+
+class GetTasksUser(BaseTaskUser):
+    @task(3)
+    def get_tasks(self):
+        """Получение списка задач."""
         self.client.get("/tasks")
